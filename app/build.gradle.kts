88c48ee8f51fe5ec2e63ecb66f31ce989853207c
Chris Warren <cwarren.dev@gmail.com>
2024-09-30 23:06:18 -0500

    updated logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,9 +66,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.google.android.gms:play-services-maps:18.0.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.google.android.gms:play-services-maps:18.0.2")
-    implementation("com.google.maps.android:maps-ktx:3.1.0")
-    implementation("com.google.maps.android:maps-utils-ktx:3.1.0")
-
 }
